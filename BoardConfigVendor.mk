0064bd9c91ee998291a6985b53277d6fa35f5bc4
Joshndroid <fmxjoshiewah@gmail.com>
2016-10-02 14:35:24 +1000

    Remove Google proprietary apps
    These are bound to GApps package

--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -12,9 +12,3 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_VENDORIMAGE_PARTITION_SIZE := 209715200
-BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE :=
-PRODUCT_COPY_FILES := \
-    device/huawei/angler/fstab.angler:root/fstab.angler \
-    $(PRODUCT_COPY_FILES)
